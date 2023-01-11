04f5cc3ccd4e3142388aa959971e6843d1fae949
Atílio Correia <Atlio Correia@NB-TI-003>
2023-01-11 08:23:45 -0300

    Add project files.

---
+++ ConversorTemperatura/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, f, k;
+
+            Console.WriteLine("Conversor de temperatura");
+            Console.Write("Insira a temperatura em Celcius: ");
+            Console.WriteLine("----------------------");
+            c = double.Parse(Console.ReadLine());
+
+            f = (c * 9 / 5) + 32;
+            k = c + 273.15;
+
+            Console.WriteLine(f + " graus celcius ");
+            Console.WriteLine(f + " graus fahrenheit");
+            Console.WriteLine(k + " graus kelvin");
+            Console.ReadKey();
+        }
+    }
+}
